c61d69fd63ebc6e10bd737325078b9f11a9e8342
ryushinn <chenliu.99@outlook.com>
2023-06-28 21:47:58 +0100

    * fix bugs

--- shell/functions.sh
+++ shell/functions.sh
@@ -8,5 +8,7 @@ mcd() {
 
 # fetch a repository and open it in VSCode
 gitcode() {
-    git clone "$1" && cd "$1" &&  code .
+    git clone "$1" && \
+    cd "$(echo "$1" | rev | cut -d'/' -f1 | cut -d'.' -f2- | rev)" && \
+    code .
 }
