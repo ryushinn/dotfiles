85e73fee506c02818a4b6e16470de560d2104296
ryushinn <chenliu.99@outlook.com>
2023-06-28 23:49:06 +0100

    * optim a bit

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 # abbr for better listing
-alias ll="ls -al"
+alias ll="ls -lh"
+alias la="ls -A"
+alias lla="la -l"
 
-# -v is the verbose option
-alias ln="ln -v"
 
-# check with man mkdir please
-alias mkdir="mkdir -p"
+alias ln="ln -v"           # -v is the verbose option
+alias mv="mv -i"           # -i prompts before overwrite
+alias mkdir="mkdir -p"     # -p make parent dirs as needed
+alias df="df -h"           # -h prints human readable format
 
 # Pretty print the path
 alias path='echo $PATH | tr -s ":" "\n"'
